9723b53603b2ad3c0c03952e4000ec53b97067d8
motakashi <takashi_home_sky@yahoo.co.jp>
2021-01-03 22:38:55 +0900

    削除・ステータス変更の処理を追加

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,16 @@
     <h1>Todoリスト</h1>
     <div class="addArea">
       <input type="text" name="addName" v-model="content" placeholder="タスクを入力してください">
-      <!-- v-on:click -->
+      <!-- ①v-on:clickにより、クリックされた場合methodにあるinsertメソッドが呼び出される -->
       <button class="button button--green" @click="insert">追加</button>
     </div>
     <div class="Filter">
-      <button class="button button--gray is-active">全て</button>
-      <button class="button button--gray">作業前</button>
-      <button class="button button--gray">作業中</button>
-      <button class="button button--gray">完了</button>
+      <!-- ①v-on:clickにより、クリックされた場合methodにあるfindメソッドが呼び出される -->
+      <!-- v-bindにより、クラスを動的に変えることができる。ここでは、is-activeがフィルタされているか、find_statusにフィルタ条件を設定-->
+      <button class="button button--gray" v-bind:class="{'is-active':(!find_flg)}" @click="flag_reset">全て</button>
+      <button class="button button--gray" v-bind:class="{'is-active': find_flg && (find_status == '作業前')}" @click="find('作業前')">作業前</button>
+      <button class="button button--gray" v-bind:class="{'is-active': find_flg && (find_status == '作業中')}" @click="find('作業中')">作業中</button>
+      <button class="button button--gray" v-bind:class="{'is-active': find_flg && (find_status == '完了')}" @click="find('完了')">完了</button>
     </div>
     <table class="Lists">
       <thead>
@@ -24,11 +26,20 @@
       </thead>
       <tbody>
         <!-- pages/index.vue -->
-        <tr v-for="(item, index) in todos" :key="index">
+        <tr v-for="(item, index) in display_todos" :key="index">
           <td>{{ item.content }}</td>
           <td>{{ item.created }}</td>
-          <td><button class="button">{{ item.state }}</button></td>
-          <td><button class="button button--delete">削除</button></td>
+          <td>
+            <button class="button" v-bind:class="{
+              'butto--yet':item.status == '作業前',
+              'butto--progress':item.status == '作業中',
+              'butto--done':item.status == '完了'}"
+              @click="changeStatus(item)">
+              {{item.state}}
+            </button>
+          </td>
+          <!-- v-on:clickにより、クリックされた場合methodにあるinsertメソッドが呼び出される -->
+          <td><button class="button button--red" @click="remove(item)">削除</button></td>
         </tr>
       </tbody>
     </table>
@@ -38,22 +49,56 @@
 import {mapState} from 'vuex';
 
 export default {
+  // v-model="content"の初期値の$store.contentの値の設定
   data: function() {
     return {
-      content:''
+      content:'',
+      find_status: '',
+      find_flg: false
     }
   },
+  // vuexからmapStateをインポートし、dataにストアのstateの中身を入れ、computedでtodosの中身が変わるたびに描写させるようにしている
   computed: {
-    ...mapState(['todos'])
+    ...mapState(['todos']),
+    display_todos: function(){
+      if(this.find_flg){
+        let arr = [];
+        let data = this.todos;
+        data.forEach(element => {
+          if(element.state == this.find_status){
+            arr.push(element);
+          }
+        });
+        return arr;
+      }else{
+        return this.todos;
+      }
+    }
   },
   methods: {
     insert: function(){
       if(this.content != ''){
-          // 
+          // storeのmutationsのinsertに、引数として入力されたタスクを渡して実行
           this.$store.commit('insert', {content: this.content});
+          // 実行後にテキストボックスの中身を空にする
           this.content = '';
       }
+    },
+    remove: function(todo){
+      this.$store.commit('remove', todo)
+    },
+    changeStatus: function(todo){
+      this.$store.commit('changeStatus', todo)
+    },
+    // vueの属性をする
+    find: function(findState){
+      this.find_status = findState;
+      this.find_flg = true;
+    },
+    flag_reset: function(){
+      this.find_flg = false;
     }
+
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -3,13 +3,20 @@ import Vuex from 'vuex';
 
 const createStore = () => {
   return new Vuex.Store({
+    // すべてのページで利用するデータをstateにまとめて管理
     state: () => ({
       todos: [
         {content: 'テスト', created: '2020-04-30 17:00', state: '作業前'}, 
         {content: 'コーディング', created: '2020-04-30 16:00', state: '作業中'},
         {content: '環境構築', created: '2020-04-30 15:30', state: '完了'}
+      ],
+      options: [
+          {id: 0, label: '作業前'},
+          {id: 1, label: '作業中'},
+          {id: 2, label: '完了'}
       ]
     }),
+    // mutationsは「変化」という意味。つまり、stateに対して値をセットできる
     mutations:{
         insert: function(state, obj){
             let createDate = new Date();
@@ -23,6 +30,36 @@ const createStore = () => {
                 created: formatCreateDate,
                 state: '作業前'
             })
+        },
+        remove: function(state, obj){
+            for (let i = 0; i < state.todos.length; i++){
+                const removeObj = state.todos[i];
+                if(obj.content == removeObj.content && obj.created == removeObj.created){
+                    if(confirm('"' + removeObj.content + '"を削除していいですか？')){
+                        state.todos.splice(i, 1);
+                        return;
+                    }
+                }
+            }
+        },
+        changeStatus: function(state, obj){
+            for (let i = 0; i < state.todos.length; i++){
+                const changeStatusObj = state.todos[i];
+                if(obj.content == changeStatusObj.content && obj.created == changeStatusObj.created){
+                    let todoStatus;
+                    for (let j = 0; j < state.options.length; j++){
+                        if(state.options[j].label == changeStatusObj.state){
+                            todoStatus = state.options[j].id;
+                        }
+                    }
+                    todoStatus = todoStatus + 1;
+                    if(todoStatus >= state.options.length){
+                        todoStatus = 0;
+                    }
+                    changeStatusObj.state = state.options[todoStatus].label;
+                    return;
+                }
+            }
         }
     }
   })
